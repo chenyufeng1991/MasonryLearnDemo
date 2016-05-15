29a797d7b1e81f3f5be336772830dd87c46f5d76
陈宇峰 <yufengcode@gmail.com>
2016-05-15 16:18:50 +0800

    Initial Commit

---
+++ MasonryLearnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryLearnDemo
+//
+//  Created by chenyufeng on 16/5/15.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasonryLearnDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasonryLearnDemo
+//
+//  Created by chenyufeng on 16/5/15.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
