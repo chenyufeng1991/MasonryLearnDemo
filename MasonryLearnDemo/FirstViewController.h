570fc30cc31aa1203ae60bcb2c93578c8ebd6a74
陈宇峰 <yufengcode@gmail.com>
2016-05-15 16:40:58 +0800

    1

--- MasonryLearnDemo/FirstViewController.h
+++ MasonryLearnDemo/FirstViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  FirstViewController.h
 //  MasonryLearnDemo
 //
 //  Created by chenyufeng on 16/5/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface FirstViewController : UIViewController
 
 @end
-
---
+++ MasonryLearnDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MasonryLearnDemo
+//
+//  Created by chenyufeng on 16/5/15.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
